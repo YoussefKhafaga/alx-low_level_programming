27a720cf7ca498ad96d78e35a898a2283d5ee30e
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-17 05:04:18 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,10 @@
  */
 void print_numbers(void)
 {
-char num;
-for (num = '0'; num <= '9'; num++)
+int num;
+for (num = 0; num <= 9; num++)
 {
-_putchar(num);
+putchar(num + '0');
 }
-_putchar('\n');
+putchar('\n');
 }
