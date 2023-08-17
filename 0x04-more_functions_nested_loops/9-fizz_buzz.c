06d59585179ad5a6c0e0e8c51775f7481b33a4c0
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-17 05:40:56 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * FizzBuzz - Prints numbers from 1 to 100 and fizz for multiple of 3 numbers
+ * and buzz for multiple of 5 numbers
+ * and fizzbuzz for multiple of 3 and 5 numbers
+ */
+void FizzBuzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+continue;
+}
+if (i % 3 == 0)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+}
+if (i % 5 == 0)
+{
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+}
+}
+}
