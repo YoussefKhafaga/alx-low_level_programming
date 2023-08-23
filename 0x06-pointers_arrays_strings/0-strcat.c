6695eb6c2a79d0aeeea83fabc96d6971f3c3357b
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-23 03:22:37 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: the final string
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i++] = src[j++];
+}
+return (dest);
+}
