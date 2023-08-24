4684213617b34279e5415db19c9a74c15424dc07
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-24 09:06:28 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - converts characters from lowercase to uppercase
+ * @str: string to be converted to uppercase
+ * Return: uppercase string
+ */
+char *string_toupper(char *str)
+{
+for (int i = 0; str[i] != '\0'; i++)
+{
+str[i] = toupper(str[i]);
+}
+return str;
+}
