fa5f419211033a20c7395636dff1ba1783a7dc6b
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-28 06:39:09 -0700

    strings pointers arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - function to fills memory with constant byte
  * @s: memory pointer
@@ -6,9 +7,11 @@
  * @n: number bytes of b
  * Return: pointer to the memory
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,6 +3,7 @@
  * _strchr - locates character in a string
  * @s: string to search for a char in
  * @c: the character to search for
+ * Return: pointer to memory location or null
  */
 char *_strchr(char *s, char c)
 {
