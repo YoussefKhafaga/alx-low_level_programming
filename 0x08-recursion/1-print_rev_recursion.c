21cb73aa37192ffcbf81d56b59fafabafdfb1e84
YoussefKhafaga <ykhfaga@gmail.com>
2023-08-30 13:22:34 -0700

    print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,13 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	        _putchar('\0');
 		return;
 	}
 	_print_rev_recursion(++s);
 	_putchar(*s);
+	if (*(s + 1) == '\0')
+	{
+		putchar('\n');
+	}
 }
 
