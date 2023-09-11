f07b9640be52d774b0be04ee1e4143e6bd8372cf
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-11 03:30:23 -0700

    new p

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,26 +8,14 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		;
-	else
+	if (d != NULL)
 	{
-		if (d->name == NULL)
+		if (d != NULL)
 		{
-			printf("nil");
+			printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+			printf("Age: %f\n", (d->age) ? d->age : 0);
+			printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 		}
-		else
-		{
-			printf("Name: %s", d->name);
-		}
-
-		printf("Age: %f", d->age);
-
-		if (d->owner == NULL)
-		{
-			printf("nil");
-		}
-		printf("Owner: %s", d->owner);
 	}
 
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,22 @@
  * @age: dog age
  * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-} dog;
+};
 
+/**
+ * dog_t - typedef for dog structure
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
