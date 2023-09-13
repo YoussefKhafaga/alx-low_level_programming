06446b824edc5775a0654bea273e9840a18846d2
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-13 03:57:17 -0700

    arr iter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prform function on each element of an array
+ * @array: array of integers
+ * @size: size of array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
+
