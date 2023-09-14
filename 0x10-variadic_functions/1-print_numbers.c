abece63687cabdea117318f817184cd79e69539d
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-14 06:21:18 -0700

    pn

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,11 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap,unsigned int));
 		if (separator)
 		{
-			printf("%s ", separator);
-		}
-		else
-		{
-			printf(" ");
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
