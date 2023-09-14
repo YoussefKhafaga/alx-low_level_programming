a6670654b73ddf10ae7c9cb6f64e62088ea98ded
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-14 06:39:38 -0700

    prnstr

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - prints strings
+ * @separator: separates strings
+ * @n: number of params
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *str;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("nil");
+		}
+		if (separator && i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
+
