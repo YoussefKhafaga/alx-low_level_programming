96872f23a34ef0be6bf5ed6f43757b1ef5d3d958
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-26 06:25:50 -0700

    poplist

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - pop the head
+ * @head: head of the list
+ * Return: value of the head
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t temp;
+	int n;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	temp = (*head)->next;
+	n = (*head)->n;
+	free(head);
+	*head = temp;
+	return (n);
+}
+
