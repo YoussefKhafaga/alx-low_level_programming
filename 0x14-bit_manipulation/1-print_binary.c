c8913d897444025fe73d209b0a9fb0691928cf3d
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-28 03:26:53 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - prints binary
+ * @n: integer
+ */
+void print_binary(unsigned long int n)
+{
+	int shift = sizeof(unsigned long int) * 8 - 1;
+	int flag = 0;
+
+	while (shift >= 0)
+	{
+		unsigned long int bit = (n >> shift) & 1;
+		if (bit)
+		{
+			flag = 1;
+		}
+		if (flag)
+		{
+			putchar(bit ? '1' : '0');
+		}
+		shift--;
+	}
+}
+
