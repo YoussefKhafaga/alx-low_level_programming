bbb8f752e8b14eb8ffb0e02d673a89ceb5d9601c
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-28 03:42:58 -0700

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - clears a bit
+ * @n: integer
+ * @index: index of the bit
+ * Return: the bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+	{
+		return (-1);
+	}
+	mask = 1 << index;
+	*n |= mask;
+	return (1);
+}
+
