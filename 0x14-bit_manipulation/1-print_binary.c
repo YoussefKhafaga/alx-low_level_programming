7b16f62f53c7b57e385e9bb91b87cefcbe36fc6e
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-28 03:29:48 -0700

    prbin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,9 +8,14 @@ void print_binary(unsigned long int n)
 	int shift = sizeof(unsigned long int) * 8 - 1;
 	int flag = 0;
 
+	if (n == 0)
+	{
+		putchar('0');
+	}
 	while (shift >= 0)
 	{
 		unsigned long int bit = (n >> shift) & 1;
+
 		if (bit)
 		{
 			flag = 1;
