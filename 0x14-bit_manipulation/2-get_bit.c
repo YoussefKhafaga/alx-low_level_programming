2e974be75b40cab5ed767b303f2b8e3676b6ea0a
YoussefKhafaga <ykhfaga@gmail.com>
2023-09-28 03:35:34 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - see function
+ * @n: int
+ * @index: index of the bit
+ * Return: bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return -1;
+	}
+
+	mask = (unsigned long int)1 << index;
+	return (n & mask) ? 1 : 0;
+}
+
